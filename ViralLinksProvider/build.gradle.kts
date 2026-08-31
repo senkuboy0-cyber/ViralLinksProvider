11420b99dbb150c621569c7c2b03c2130ffa4e32
senkuboy0-cyber <255331092+senkuboy0-cyber@users.noreply.github.com>
2026-08-31 22:23:24 +0600

    Fix isCrossPlatform = false for Android-only plugin with WebView

--- ViralLinksProvider/build.gradle.kts
+++ ViralLinksProvider/build.gradle.kts
@@ -23,7 +23,7 @@ cloudstream {
         "TvSeries",
         "Movie"
     )
-    isCrossPlatform = true
+    isCrossPlatform = false
     requiresResources = true
     iconUrl = "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhQvNXfZt7ctszD6Fy_FwU7NfcyxIEZ6uW6asTw_5cMPS38hkm65bQdzb2bCD-86XfOUVmp5xjOANaefT4ZdWSCf_picqYtsAN5McX_3gVEfdVa5EA4h9e2noiaNLwUhMK8VaGx1mQGI_7TCnpmEI3LxtgNPeVpKsojjSbqSZh50VbyrTiP7_2KOIusBBsC/s1024/1000073990.png"
 }
