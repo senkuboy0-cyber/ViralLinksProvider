42ae474a49869852c6100c029e6dedd1b1e1ecf9
senkuboy0-cyber <255331092+senkuboy0-cyber@users.noreply.github.com>
2026-08-31 22:10:52 +0600

    Add AndroidX Material dependency for BottomSheetDialogFragment

--- ViralLinksProvider/build.gradle.kts
+++ ViralLinksProvider/build.gradle.kts
@@ -4,6 +4,16 @@ plugins {
 
 version = 1
 
+android {
+    buildFeatures {
+        buildConfig = true
+    }
+}
+
+dependencies {
+    implementation("com.google.android.material:material:1.14.0")
+}
+
 cloudstream {
     language = "en"
     authors = listOf("Cloudstream Extension")
@@ -14,5 +24,6 @@ cloudstream {
         "Movie"
     )
     isCrossPlatform = true
+    requiresResources = true
     iconUrl = "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhQvNXfZt7ctszD6Fy_FwU7NfcyxIEZ6uW6asTw_5cMPS38hkm65bQdzb2bCD-86XfOUVmp5xjOANaefT4ZdWSCf_picqYtsAN5McX_3gVEfdVa5EA4h9e2noiaNLwUhMK8VaGx1mQGI_7TCnpmEI3LxtgNPeVpKsojjSbqSZh50VbyrTiP7_2KOIusBBsC/s1024/1000073990.png"
 }
