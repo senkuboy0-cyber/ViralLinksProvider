22100e2b60bc9726156e5e5da36b0634919054c1
senkuboy0-cyber <255331092+senkuboy0-cyber@users.noreply.github.com>
2026-08-31 21:39:25 +0600

    Update MusicbdPlugin with settings support

--- ViralLinksProvider/src/main/kotlin/com/musicbd/MusicbdPlugin.kt
+++ ViralLinksProvider/src/main/kotlin/com/musicbd/MusicbdPlugin.kt
@@ -1,11 +1,29 @@
 package com.musicbd
-import com.lagradost.cloudstream3.plugins.BasePlugin
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 
-
 @CloudstreamPlugin
-class MusicbdPlugin: BasePlugin() {
+class MusicbdPlugin : com.lagradost.cloudstream3.plugins.BasePlugin() {
+    
     override fun load() {
         registerMainAPI(MusicbdProvider())
+    }
+
+    init {
+        // Enable settings button in CloudStream app
+        this.openSettings = { activity ->
+            try {
+                val act = activity as? AppCompatActivity
+                if (act != null) {
+                    val mainUrl = "https://musicbd25.site"
+                    val settingsFragment = MusicbdSettingsFragment(this, mainUrl)
+                    settingsFragment.show(act.supportFragmentManager, "Musicbd25Settings")
+                }
+            } catch (e: Exception) {
+                // Handle error silently
+            }
+        }
     }
 }
