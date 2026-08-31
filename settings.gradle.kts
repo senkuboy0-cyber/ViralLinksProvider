f86418dfd896f89c1a280f711744cb5ec72fca3c
senkuboy0-cyber <255331092+senkuboy0-cyber@users.noreply.github.com>
2026-08-31 19:54:52 +0600

    Update settings.gradle.kts with proper configuration

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,21 @@
 rootProject.name = "ViralLinksProvider"
+
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+        maven("https://jitpack.io")
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
+}
+
 include(":ViralLinksProvider")
